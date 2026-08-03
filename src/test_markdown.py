3be0b50a76e2609005fd4db84bbe39dfb1f2ab13
Erynik <rynkir@gmail.com>
2026-08-03 21:02:16 +0100

    naming issues

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, markdown_to_blocks, markdown_to_html_node
+from markdown_code import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes, markdown_to_blocks, markdown_to_html_node
 from textnode import TextNode, TextType
 
 class TestMarkDown(unittest.TestCase):
